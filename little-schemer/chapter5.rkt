53f7b841b0d3549d9e730534ef6ab4297b86a103
fucangyu <fucangyu@ein.plus>
2018-11-09 21:56:53 +0800

    rember*

--- little-schemer/chapter5.rkt
+++ little-schemer/chapter5.rkt
@@ -1,3 +1,12 @@
 #lang racket
 
+(define (rember* a l)
+  (cond
+    [(null? l) '()]
+    [(pair? (car l)) (cons (rember* a (car l))
+                           (rember* a (cdr l)))]
+    [(eqv? a (car l)) (rember* a (cdr l))]
+    [else (cons (car l)
+                (rember* a (cdr l)))]))
+
 
