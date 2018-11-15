2460d25a92dd885b1dc009fcc03732872d4b37b5
fucangyu <fucangyu@ein.plus>
2018-11-15 23:25:14 +0800

    add 2.36

--- sicp/sicp2.rkt
+++ sicp/sicp2.rkt
@@ -229,3 +229,11 @@
                                           (count-leaves subtree)
                                           1)) t)))
 
+
+;; 2.36
+
+(define (accumulate-n op init seqs)
+  (if (null? (car seqs))
+      '()
+      (cons (accumulate op init (map car seqs))
+            (accumulate-n op init (map cdr seqs)))))
